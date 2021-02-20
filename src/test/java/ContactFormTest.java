0a2ce90d8e511f6aba6681e16b959fbf77cc96d8
Nagalakshmi Selvaraj <lakshmi.padumanabhan@gmail.com>
2021-02-20 13:40:53 +0530

    Selenium,TestNG and WebDriverManager dependencies added

---
+++ src/test/java/ContactFormTest.java
@@ -0,0 +1,12 @@
+import org.testng.annotations.Test;
+
+@Test
+public class ContactFormTest {
+
+    @Test
+    public void contactFormValidation() {
+
+        System.out.println("test");
+    }
+
+}
