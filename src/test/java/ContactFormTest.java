ff118cff597b12a5f67cbb7cb1cda099264838ce
Nagalakshmi Selvaraj <lakshmi.padumanabhan@gmail.com>
2021-02-20 16:40:50 +0530

    Test case 2 - Verify form validation and successful submission message has been added

--- src/test/java/ContactFormTest.java
+++ src/test/java/ContactFormTest.java
@@ -33,4 +33,30 @@ public class ContactFormTest {
         chromeDriver.close();
     }
 
+    @Test(description = "Test Case 2 - Verify form validation and successful submission message", priority = 2)
+    public void contactFormSubmission() throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver chromeDriver = new ChromeDriver();
+        chromeDriver.manage().window().maximize();
+        chromeDriver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+        chromeDriver.get("https://jupiter.cloud.planittesting.com/");
+        chromeDriver.findElement(By.id("nav-contact")).click();
+        WebDriverWait webDriverWait = new WebDriverWait(chromeDriver, 20);
+        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.className("btn-primary")));
+        chromeDriver.findElement(By.className("btn-primary")).click();
+        Assert.assertEquals(chromeDriver.findElement(By.id("forename-err")).getText(),"Forename is required");
+        Assert.assertEquals(chromeDriver.findElement(By.id("email-err")).getText(),"Email is required");
+        Assert.assertEquals(chromeDriver.findElement(By.id("message-err")).getText(),"Message is required");
+        chromeDriver.findElement(By.id("forename")).sendKeys("Nagalakshmi");
+        chromeDriver.findElement(By.id("email")).sendKeys("lakshmi.padumanabhan@gmail.com");
+        chromeDriver.findElement(By.id("message")).sendKeys("planit automation testing");
+        chromeDriver.findElement(By.className("btn-primary")).click();
+        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.className("alert-success")));
+        Assert.assertEquals(chromeDriver.findElement(By.className("alert-success")).getText(),"Thanks Nagalakshmi, we appreciate your feedback.");
+        Thread.sleep(2000);
+        chromeDriver.close();
+    }
+
+
+
 }
