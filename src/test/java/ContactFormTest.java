1f6b7eaac051446897917fe10659073c0566d750
Nagalakshmi Selvaraj <lakshmi.padumanabhan@gmail.com>
2021-02-20 13:54:28 +0530

    Application launch funtionality implemented

--- src/test/java/ContactFormTest.java
+++ src/test/java/ContactFormTest.java
@@ -1,12 +1,18 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 @Test
 public class ContactFormTest {
 
-    @Test
+    @Test(description = "Test Case 1 - Verify form validation messages", priority = 1)
     public void contactFormValidation() {
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver chromeDriver = new ChromeDriver();
+        chromeDriver.manage().window().maximize();
+        chromeDriver.get("https://jupiter.cloud.planittesting.com/");
+        chromeDriver.close();
 
-        System.out.println("test");
     }
 
 }
